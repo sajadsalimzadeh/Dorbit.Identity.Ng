f9bb5917cd02cef76284d5ae7c27fffd252a5904
sajad <salimzadehsajad@gmail.com>
2024-03-15 14:59:27 +0330

    improve user reset pass license

--- src/pages/users/index.module.ts
+++ src/pages/users/index.module.ts
@@ -5,6 +5,7 @@ import {IndexComponent} from './index.component';
 import {FormComponent} from "./form/index.component";
 import {AccessComponent} from "./access/index.component";
 import {IdentitySharedModule} from "../../components";
+import {ResetPasswordComponent} from "./reset-password/index.component";
 
 @NgModule({
   imports: [
@@ -12,7 +13,7 @@ import {IdentitySharedModule} from "../../components";
     RouterModule.forChild([{path: '', component: IndexComponent}]),
   ],
   exports: [FormComponent],
-  declarations: [IndexComponent, FormComponent, AccessComponent],
+  declarations: [IndexComponent, FormComponent, AccessComponent, ResetPasswordComponent],
   providers: [],
 })
 export class IndexModule {
---
+++ src/pages/users/reset-password/index.component.ts
@@ -0,0 +1,35 @@
+import {Component, EventEmitter, Injector, Input, Output} from '@angular/core';
+import {UserRepository} from "../../../repositories";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {BasePanelComponent} from "@panel";
+
+@Component({
+  selector: 'app-reset-password',
+  templateUrl: 'index.component.html',
+  styleUrls: ['./index.component.scss']
+})
+export class ResetPasswordComponent extends BasePanelComponent {
+
+  @Input({required: true}) model: any;
+
+  @Output() onComplete = new EventEmitter<void>();
+
+  form = new FormGroup({
+    password: new FormControl('', [Validators.required])
+  })
+
+  constructor(injector: Injector, private userRepository: UserRepository) {
+    super(injector);
+  }
+
+  override ngOnInit() {
+    super.ngOnInit();
+  }
+
+  save() {
+    this.userRepository.resetPassword({id: this.model.id, ...this.form.value}).subscribe(res => {
+      this.messages.success();
+      this.onComplete.emit();
+    });
+  }
+}
