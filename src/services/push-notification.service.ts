4e243a27537d0af8e9cd4add747e886c7b22f5e3
sajad <salimzadehsajad@gmail.com>
2025-07-23 02:13:57 +0330

    feature: web push implemented

--- src/services/push-notification.service.ts
+++ src/services/push-notification.service.ts
@@ -7,8 +7,20 @@ import { SwPush } from '@angular/service-worker';
 export class PushNotificationService {
 
     constructor(private userRepository: UserRepository, private swPush: SwPush) {
-        this.swPush.messages.subscribe(message => {
+        this.swPush.messages.subscribe((message: any) => {
             console.log('Push message received:', message);
+            Notification.requestPermission().then(permission => {
+                if (permission === 'granted') {
+                    message.title ??= 'Trainout';
+                    message.body ??= 'New message';
+                    message.icon ??= '/assets/images/logo.png';
+                    message.data ??= {
+                        url: '/',
+                    };
+
+                    new Notification(message.title, { body: message.body, icon: message.icon, data: message.data });
+                }
+            });
         });
     }
 
