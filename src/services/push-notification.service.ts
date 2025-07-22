021a34b92cbebfd4fb6a242d61bc1cc3f3ef9850
sajad <salimzadehsajad@gmail.com>
2025-07-23 02:02:51 +0330

    feature: web push implemented

--- src/services/push-notification.service.ts
+++ src/services/push-notification.service.ts
@@ -10,35 +10,19 @@ export class PushNotificationService {
 
     async subscribeToNotifications() {
         const permission = await Notification.requestPermission();
-        
         if (permission !== 'granted') return;
-
-        const registration = await navigator.serviceWorker.ready;
-        const subscription = await registration.pushManager.subscribe({
-            userVisibleOnly: true,
-            applicationServerKey: this.urlBase64ToUint8Array(app.settings.webpush.publicKey),
-        });
         this.swPush.requestSubscription({
-          serverPublicKey: app.settings.webpush.publicKey
-        }).then(sub => {
-          this.userRepository.setOwnWebPushSubscription(sub).subscribe();
+            serverPublicKey: app.settings.webpush.publicKey
+        }).then(subscription => {
+            console.log('web-push', subscription);
+
+            const subObject = subscription.toJSON();
+            const req = {
+                endPoint: subObject.endpoint,
+                p256dh: subObject.keys?.['p256dh'] ?? null,
+                auth: subObject.keys?.['auth'] ?? null,
+            }
+            this.userRepository.setOwnWebPushSubscription(req).subscribe();
         });
-
-        console.log('web-push', subscription);
-
-        const subObject = subscription.toJSON();
-        const req = {
-            endPoint: subObject.endpoint,
-            p256dh: subObject.keys?.['p256dh'] ?? null,
-            auth: subObject.keys?.['auth'] ?? null,
-        }
-        this.userRepository.setOwnWebPushSubscription(req).subscribe();
-    }
-
-    private urlBase64ToUint8Array(base64String: string): Uint8Array {
-        const padding = '='.repeat((4 - base64String.length % 4) % 4);
-        const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/');
-        const rawData = atob(base64);
-        return new Uint8Array([...rawData].map(char => char.charCodeAt(0)));
     }
 }
