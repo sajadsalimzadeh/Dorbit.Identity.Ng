32788016e75b43f839ddd42f029bda06e73d6eae
sajad <salimzadehsajad@gmail.com>
2025-07-23 01:47:04 +0330

    feature: web push implemented

--- src/services/push-notification.service.ts
+++ src/services/push-notification.service.ts
@@ -8,31 +8,25 @@ export class PushNotificationService {
     constructor(private userRepository: UserRepository) { }
 
     async subscribeToNotifications() {
-        const applicationServerKey = this.urlBase64ToUint8Array(app.settings.webpush.publicKey);
-        navigator.serviceWorker.register('push-sw.js')
-            .then(registration => {
-                console.log('Service Worker Registered!', registration);
-                registration.pushManager.getSubscription().then(async sub => {
-                    if (sub === null) {
-                        const registration = await navigator.serviceWorker.ready;
-                        const subscription = await registration.pushManager.subscribe({
-                            userVisibleOnly: true,
-                            applicationServerKey: applicationServerKey,
-                        });
+        const permission = await Notification.requestPermission();
+        
+        if (permission !== 'granted') return;
 
-                        console.log('web-push', subscription);
+        const registration = await navigator.serviceWorker.ready;
+        const subscription = await registration.pushManager.subscribe({
+            userVisibleOnly: true,
+            applicationServerKey: this.urlBase64ToUint8Array(app.settings.webpush.publicKey),
+        });
 
-                        const subObject = subscription.toJSON();
-                        const req = {
-                            endPoint: subObject.endpoint,
-                            p256dh: subObject.keys?.['p256dh'] ?? null,
-                            auth: subObject.keys?.['auth'] ?? null,
-                        }
-                        this.userRepository.setOwnWebPushSubscription(req).subscribe();
-                    }
-                });
-            })
-            .catch(error => console.error('Service Worker Error:', error));
+        console.log('web-push', subscription);
+
+        const subObject = subscription.toJSON();
+        const req = {
+            endPoint: subObject.endpoint,
+            p256dh: subObject.keys?.['p256dh'] ?? null,
+            auth: subObject.keys?.['auth'] ?? null,
+        }
+        this.userRepository.setOwnWebPushSubscription(req).subscribe();
     }
 
     private urlBase64ToUint8Array(base64String: string): Uint8Array {
