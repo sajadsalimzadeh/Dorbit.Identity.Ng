7bbbdc6072168f24295f86e413561d7d22bb1adc
sajad <salimzadehsajad@gmail.com>
2026-05-29 20:58:15 +0330

    improve: access selecor

--- components/access-selector/index.component.ts
+++ components/access-selector/index.component.ts
@@ -1,23 +1,35 @@
 import { CommonModule } from "@angular/common";
-import { Component, Injector, Input } from "@angular/core";
-import { FormControl, ReactiveFormsModule } from "@angular/forms";
+import { Component, forwardRef, Injector, Input } from "@angular/core";
+import { FormControl, NG_VALUE_ACCESSOR, ReactiveFormsModule } from "@angular/forms";
+import { TreeUtil } from "@framework/utils";
+import { Access } from "@identity/contracts/access";
 import { AccessRepository } from "@identity/repositories/access.repository";
 import { TranslateModule } from "@ngx-translate/core";
-import { PrimengComponent } from "@primeng/components/primeng.component";
+import { PrimengControlComponent } from "@primeng/components/primeng-control.component";
 import { TreeNode } from "primeng/api";
 import { IftaLabelModule } from "primeng/iftalabel";
 import { TreeSelectModule } from "primeng/treeselect";
+
+let isLoadingAccesses = false;
+const listeners: ((accesses: Access[]) => void)[] = [];
 
 @Component({
     standalone: true,
     imports: [CommonModule, ReactiveFormsModule, TranslateModule, TreeSelectModule, IftaLabelModule],
     selector: 'app-access-selector',
     templateUrl: './index.component.html',
-    styleUrl: './index.component.scss'
+    styleUrl: './index.component.scss',
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            useExisting: forwardRef(() => AccessSelectorComponent),
+            multi: true
+        }
+    ]
 })
-export class AccessSelectorComponent extends PrimengComponent {
+export class AccessSelectorComponent extends PrimengControlComponent {
     @Input() label: string = 'access-selector.label';
-    @Input({ required: true }) control!: FormControl<string[] | null>;
+    @Input() filterBy: string[] = [];
 
     innerControl = new FormControl<TreeNode[]>([]);
 
@@ -28,26 +40,56 @@ export class AccessSelectorComponent extends PrimengComponent {
     }
 
     override ngOnInit() {
-        this.subscription.add(this.accessRepository.getAll().subscribe(res => {
-            const accesses = res.data ?? [];
-            this.nodes = accesses.map(access => ({
-                key: access.id,
-                label: access.name,
-                data: access,
-            }));
-
-            const selectedAccesses = this.control.value ?? [];
-            this.nodes.forEach(node => {
-                node.checked = selectedAccesses.includes(node.data.name);
-                node.children = this.nodes.filter(n => n.data.parentId === node.key);
-            });
-            this.innerControl.setValue(this.nodes.filter(node => node.checked));
-
-            this.nodes = this.nodes.filter(node => !node.data.parentId);
-        }));
+        this.load();
 
         this.subscription.add(this.innerControl.valueChanges.subscribe(value => {
-            this.control.setValue(value?.map(v => v.data.name) ?? []);
+            this.onChange?.(value?.map(v => v.data.name) ?? []);
         }));
+    }
+
+    load() {
+        if (isLoadingAccesses) {
+            listeners.push((accesses) => this.process(accesses));
+            return;
+        }
+        isLoadingAccesses = true;
+        this.subscription.add(this.accessRepository.getAll().subscribe(res => {
+            isLoadingAccesses = false;
+            const accesses = res.data ?? [];
+            listeners.forEach(listener => listener(accesses));
+            listeners.clear();
+            this.process(accesses);
+        }));
+    }
+
+    process(accesses: Access[]) {
+        this.nodes = accesses.map(access => ({
+            key: access.id,
+            label: access.name,
+            data: access,
+        }));
+        this.nodes.forEach(node => {
+            node.children = this.nodes.filter(n => n.data.parentId === node.key);
+        });
+
+        const selectedAccesses = this.value ?? [];
+        this.nodes = this.nodes.filter(node => !node.data.parentId);
+
+        const filters = this.filterBy.map(filter => filter.toLowerCase());
+        if (filters.length > 0) {
+            this.nodes = this.nodes.filter(node => filters.includes(node.data.name.toLowerCase()));
+        }
+
+        const selectedNodes: TreeNode[] = [];
+        TreeUtil.iterate(this.nodes, node => {
+            node.checked = selectedAccesses.includes(node.data.name);
+            if (node.checked) {
+                selectedNodes.push(node);
+            }
+        });
+        if (this.nodes.length === 1) {
+            this.nodes[0].expanded = true;
+        }
+        this.innerControl.setValue(selectedNodes);
     }
 }
